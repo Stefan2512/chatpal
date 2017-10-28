9106b8d7b17189acc9b27c7d8658062e720827d6
stefan2512 <mircea.badragan@iwelcome.com>
2017-10-28 13:46:41 +0300

    eliza dictionary now contains a list; example of an anwser

--- eliza.go
+++ eliza.go
@@ -2,43 +2,34 @@ package main
 
 import (
 	"regexp"
+	"fmt"
 )
 
 // possible statements
-var ps = map[string]map[string]string{
-	"bored": {
-		"somet": "someth",
-		"somet2": "someth2",
-		"somet3": "someth3",
-		"somet4": "someth4",
-	},
-	"inspired": {
-		"da": "nu",
-	},
-	"expensive": {
-
-	},
-	"cheap": {
-
+var ps = map[string][]string{
+	"I need ?(.*)": {
+		"Why do you need %s?",
+		"Do you really need {0}?",
 	},
 	"fallbackStatements": {
-		"okay": "you must be really bored...",
+		"Don't know what to say about this",
 	},
 }
 
-func parseMessageAndGetContext(s string) map[string]string{
+func parseMessageAndGetContext(s string) string {
 	for k := range ps {
 		re := regexp.MustCompile(k)
-		if re.FindString(s) == k {
-			return ps[k]
+		findings := re.FindStringSubmatch(s)
+		if findings != nil {
+			interpolatedAnswer := fmt.Sprintf(ps[k][0], findings[1])
+			return interpolatedAnswer
 		}
 	}
 	// if no other keys are parsed, fallback to some default statements
-	return ps["fallbackStatements"]
+	return ps["fallbackStatements"][0]
 }
 
-
-func randomizeMessage(context map[string]string) string{
+func randomizeMessage(context map[string]string) string {
 	// go lang implementation iterates randomly through the keys
 	// TODO: should enhance this
 	for k := range context {
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	//"net/http"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	print("Listening on port 4419...")
+	print(parseMessageAndGetContext("I need some water"))
 	http.ListenAndServe(":4419", nil)
 }
