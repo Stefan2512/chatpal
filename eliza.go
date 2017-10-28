c186971ae21ef024e1bcc1169a0a55b2d4eef718
stefan2512 <mircea.badragan@iwelcome.com>
2017-10-28 14:03:12 +0300

    if message contains a key in eliza dictionary, response with a random message in dictionary

--- eliza.go
+++ eliza.go
@@ -3,37 +3,44 @@ package main
 import (
 	"regexp"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // possible statements
 var ps = map[string][]string{
 	"I need ?(.*)": {
 		"Why do you need %s?",
-		"Do you really need {0}?",
+		"Do you really need %s?",
 	},
 	"fallbackStatements": {
 		"Don't know what to say about this",
 	},
 }
 
+
+// try to parse with every key in the eliza dictionary,
+// if so happens, pick a random predefined message in the dictionary
 func parseMessageAndGetContext(s string) string {
 	for k := range ps {
 		re := regexp.MustCompile(k)
 		findings := re.FindStringSubmatch(s)
 		if findings != nil {
-			interpolatedAnswer := fmt.Sprintf(ps[k][0], findings[1])
+			randomMessageIndex := randomizeMessage(len(ps[k]))
+			interpolatedAnswer := fmt.Sprintf(ps[k][randomMessageIndex], findings[1])
 			return interpolatedAnswer
 		}
 	}
-	// if no other keys are parsed, fallback to some default statements
-	return ps["fallbackStatements"][0]
+
+	// if no other keys are found, fallback to some default statements
+	fallBackKey := "fallbackStatements"
+	randomFallbackMessageIndex := randomizeMessage(len(ps[fallBackKey]))
+	return ps[fallBackKey][randomFallbackMessageIndex]
 }
 
-func randomizeMessage(context map[string]string) string {
-	// go lang implementation iterates randomly through the keys
-	// TODO: should enhance this
-	for k := range context {
-		return context[k]
-	}
-	return "nil"
+func randomizeMessage(n int) int {
+	// appropriately seed the pseudo-random generator
+	// so that at every message to get a different response
+	rand.Seed(time.Now().UTC().UnixNano())
+	return rand.Intn(n)
 }
